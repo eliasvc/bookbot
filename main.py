77ea567b209ba3a6d9642bff207eb10f588919c4
Elias <elias.vc@gmail.com>
2024-03-05 21:29:12 -0500

    Added sort function and printing report in reverse sorted order

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def sort_on(dict):
+    """Sorting function for a dictionary using key 'num'"""
+    return dict["num"]
+
 def count_words(text):
     """Return the amount of words in a text"""
     return len(text.split())
@@ -21,6 +25,13 @@ def main():
         
         print(f"--- Begin report of {book_path} ---")
         print(f"{count_words(file_contents)} words found in document\n\n")
+
+        # Only interested in the letters of the alphabet
+        alpha_only = [{"char": char, "num": num} for char, num in count_letters(file_contents).items() if char.isalpha()]
+        alpha_only.sort(reverse=True, key=sort_on)
+        for letter in alpha_only:
+            print(f"The {letter['char']} character was found {letter['num']} times")
+        print ("--- End report---")
 
 if __name__ == "__main__":
     main()
